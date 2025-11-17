ce52f7d47f566ae0449dc511b78a37ff510a5a97
Alvin Dennis <alvindennis80@gmail.com>
2025-11-17 21:22:52 +0530

    feat: remove images export

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compress: true,
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
